3f8fd43642a52adcd4f9d4985afae95428a49e2c
Manoj Kavithal <manojkothwal9223@gmail.com>
2021-11-20 21:22:30 +0530

    Adding changes to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 # Calculator library containing basic math operations #
 def add(first_term, second_term):
     return first_term + second_term
+    
 
-
-def subtraction(first_term, second_term):
+def sub(first_term, second_term):
     return first_term - second_term
