9bca78dc2d144d54701d59cb050593d45c79ebc6
Manoj Kavithal <manojkothwal9223@gmail.com>
2021-11-20 21:34:49 +0530

    File changed finally !!

--- caculator.py
+++ caculator.py
@@ -5,7 +5,7 @@ def add(first_term, second_term):
 
 def minus(first_term, second_term):
     return first_term - second_term
-    
+
 
 def test_multiply(self):
     	return first_term * second_term
