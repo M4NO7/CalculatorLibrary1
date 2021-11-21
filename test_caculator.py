44dab289ab6fd43934c74268a4f7d4528570cda5
Manoj Kavithal <manojkothwal9223@gmail.com>
2021-11-21 22:59:48 +0530

    4

--- test_caculator.py
+++ test_caculator.py
@@ -7,15 +7,18 @@ import calculator
 
 class TestCalculator:
 
-    def test_addition(self):
+   def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
-    def test_subtract(self):
+
+   def test_subtract(self):
         assert 2 == calculator.subtract(4, 2)
+
 
    def test_multiply(self):
     assert 100 == calculator.multiply(10, 10)
 
+
    def test_divide(self):
    	assert 81 == calculator.divide(243, 3)
-   	
+
